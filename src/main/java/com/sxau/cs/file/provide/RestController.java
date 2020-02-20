dd068c240aadc51991660a40fa9601825846241f
libo <3214640369@qq.com>
2020-02-20 21:38:06 +0800

    修改RestCotroller

--- src/main/java/com/sxau/cs/file/provide/RestController.java
+++
@@ -1,8 +0,0 @@
-package com.sxau.cs.file.provide;
-
-public interface RestController {
-    String nameLogin(String json);
-    String logout(String json);
-    String download(String json);
-    String upload(String json);
-}
