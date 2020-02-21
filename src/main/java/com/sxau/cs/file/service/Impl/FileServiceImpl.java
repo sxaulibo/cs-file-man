bb63577c45f978e18b25299fc2179808f29a2ad2
xuedisong <1583477527@qq.com>
2020-02-21 17:38:44 +0800

    add annotation of service

--- src/main/java/com/sxau/cs/file/service/Impl/FileServiceImpl.java
+++ src/main/java/com/sxau/cs/file/service/Impl/FileServiceImpl.java
@@ -1,6 +1,9 @@
 package com.sxau.cs.file.service.Impl;
 
 import com.sxau.cs.file.service.FileService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FileServiceImpl implements FileService {
+
 }
