5238b074aba486d1924d8e7fc1e73d5c0c80547c
xuedisong <1583477527@qq.com>
2020-02-29 05:04:33 +0800

    add module in pom

---
+++ provider/src/main/java/com/sxau/cs/file/provider/constants/Constant.java
@@ -0,0 +1,20 @@
+package com.sxau.cs.file.provider.constants;
+
+public interface Constant {
+
+    /**
+     * 响应状态描述
+     */
+    interface Stat {
+
+        /**
+         * 成功
+         */
+        String OK = "OK";
+
+        /**
+         * 错误
+         */
+        String ERROR = "ERROR";
+    }
+}
