5b786fe14dacd5bb5c825966f709fef5b4e28426
xuedisong <1583477527@qq.com>
2020-03-02 23:50:58 +0800

    add spring-config.xml springmvc-servlet.xml

--- provider/src/main/java/com/sxau/cs/file/man/provider/FileController.java
+++ provider/src/main/java/com/sxau/cs/file/man/provider/FileController.java
@@ -6,34 +6,30 @@ import com.sxau.cs.file.man.common.model.request.FileListRequest;
 import com.sxau.cs.file.man.common.model.request.FilePathRequest;
 import com.sxau.cs.file.man.common.model.response.FileInfoResponse;
 import com.sxau.cs.file.man.common.model.response.FileListResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.annotation.Resource;
+
+@RestController
 public class FileController {
 
-    @Autowired
+    @Resource
     private FileBiz fileBiz;
 
-    @ResponseBody
     public FileInfoResponse infoByPath(@RequestBody FilePathRequest filePathRequest) {
         return fileBiz.infoByPath(filePathRequest);
     }
 
-    @ResponseBody
     public FileListResponse list(@RequestBody FileListRequest fileListRequest) {
         return fileBiz.list(fileListRequest);
     }
 
-    @ResponseBody
     public FileInfoResponse createDir(@RequestBody FileCreateRequest fileCreateRequest) {
         return fileBiz.createDir(fileCreateRequest);
     }
 
-    @ResponseBody
     public void download(@RequestParam Long fid, String token) {
         fileBiz.download(fid, token);
     }
